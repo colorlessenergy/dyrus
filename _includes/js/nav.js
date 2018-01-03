82a02fe2fc70cc00d0a667d448c962c4764afa65
colorlessenergy <brianmunozdev@gmail.com>
2018-01-02 19:06:38 -0800

    animation add to the nav

--- _includes/js/nav.js
+++ _includes/js/nav.js
@@ -2,8 +2,12 @@ var navMenu = document.querySelector('#nav-toggle');
 
 
 navMenu.addEventListener('click', function () {
-    let slideAnimation = document.querySelector(".mobilemenu")
-    slideAnimation.classList.toggle("mobilemenu--animation");
+
+    let iconMenu = document.querySelector('#icon-hamburger');
+    iconMenu.classList.toggle('rect');
+
+    let slideAnimation = document.querySelector('.mobilemenu')
+    slideAnimation.classList.toggle('mobilemenu--animation');
     
     console.log(slideAnimation)
 })
